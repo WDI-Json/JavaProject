808c382e7ec15974ae6b430bddc5c16ec80b533c
WDI-Json <30670162+WDI-Json@users.noreply.github.com>
2022-04-19 16:50:53 +0200

    Working basic MVC architecture Java webapp

---
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/WindesheimDemoApplication.java
@@ -0,0 +1,13 @@
+package routeplannerpoc.windesheimdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindesheimDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindesheimDemoApplication.class, args);
+	}
+
+}
---
+++ WindesheimDemo/src/test/java/RouteplannerPOC/WindesheimDemo/WindesheimDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package RouteplannerPOC.WindesheimDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindesheimDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
