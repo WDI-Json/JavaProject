e0a275fb1a55c3f6f10254e3cbbb2c84d98e26e6
WDI_JSON <wouterdijks@hotmail.com>
2022-05-03 19:46:02 +0200

    werkend POST concept

--- WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/controller/GreetingController.java
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/controller/GreetingController.java
@@ -3,15 +3,24 @@ package routeplannerpoc.windesheimdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import routeplannerpoc.windesheimdemo.model.Greeting;
 
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
+  @GetMapping("/greeting")
+  public String greetingForm(Model model) {
+    model.addAttribute("greeting", new Greeting());
+    return "greeting";
+  }
+
+  @PostMapping("/greeting")
+  public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+    model.addAttribute("greeting", greeting);
+    return "result";
+  }
 
 }
---
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/model/Greeting.java
@@ -0,0 +1,34 @@
+package routeplannerpoc.windesheimdemo.model;
+
+
+public class Greeting {
+
+  private long id;
+  private String content;
+  private String cityname;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+public String getCityname() {
+    return cityname;
+}
+
+public void setCityname(String name) {
+  this.cityname = name;
+}
+
+}
