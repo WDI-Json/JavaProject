aaa08e345483a78d04345c872f6e652bf6b50db6
WDI_JSON <wouterdijks@hotmail.com>
2022-05-06 13:00:40 +0200

    werkende responsbody met correcte JSON

--- WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/controller/GreetingController.java
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/controller/GreetingController.java
@@ -8,22 +8,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.micrometer.core.ipc.http.HttpSender.Method;
+// import io.micrometer.core.ipc.http.HttpSender.Method;
 import routeplannerpoc.windesheimdemo.model.Greeting;
 
 
 
 
 @Controller
-// @ResponseBody
+
 @RestController
 public class GreetingController {
 
@@ -34,10 +34,16 @@ public class GreetingController {
   }
 
   @RequestMapping(value = "/greeting", method = RequestMethod.POST, produces = "application/json")
+  @ResponseBody()
   public ResponseEntity<String> greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
     model.addAttribute("greeting", greeting);
-    return new ResponseEntity<String>("[{title: 'Hogeschool Windesheim,location': { lat: 52.49953, lng: 6.07845 }},{title: 'Engelse werk',location: { lat: 52.4970232, lng: 6.06394 }},{title: 'Scania',location: { lat: 52.5255141, lng: 6.0800041 }},{title: 'McDonalds Noord',location: { lat: 52.5224281, lng: 6.1145818 }},{title: 'Hogeschool Windesheim',location: { lat: 52.49953, lng: 6.07845 }}]" ,
-	HttpStatus.OK);
+    return new ResponseEntity<String>("""
+    [{"title": "Hogeschool Windesheim", "location": { "lat": 52.49953, "lng": 6.07845 }},
+    {"title": "Engelse werk","location": { "lat": 52.4970232, "lng": 6.06394 }},
+    {"title": "Scania","location": { "lat": 52.5255141, "lng": 6.0800041 }},
+    {"title": "McDonalds Noord", "location": { "lat": 52.5224281, "lng": 6.1145818 }},
+    {"title": "Hogeschool Windesheim", "location": { "lat": 52.49953, "lng": 6.07845 }}]""",
+    HttpStatus.OK);
   }
   
   @RequestMapping("/test")
