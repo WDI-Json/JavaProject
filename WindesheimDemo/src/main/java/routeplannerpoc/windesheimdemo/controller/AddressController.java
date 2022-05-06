572814e23e1b717025592ab24c50717a7068fd86
WDI_JSON <wouterdijks@hotmail.com>
2022-05-06 13:00:14 +0200

    comment voor later

--- WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/controller/AddressController.java
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/controller/AddressController.java
@@ -1,41 +1,41 @@
-package routeplannerpoc.windesheimdemo.controller;
+// package routeplannerpoc.windesheimdemo.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.JsonNode;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
 
-import routeplannerpoc.windesheimdemo.geocoder.Geocoder;
-import routeplannerpoc.windesheimdemo.model.Address;
+// import routeplannerpoc.windesheimdemo.geocoder.Geocoder;
+// import routeplannerpoc.windesheimdemo.model.Address;
 
-@Controller
-public class AddressController {
+// @Controller
+// public class AddressController {
     
-    public String convertToGeo(Address address) {
-        String geocode;
-        ObjectMapper mapper = new ObjectMapper();
-        Geocoder geocoder = new Geocoder();
+//     public String convertToGeo(Address address) {
+//         String geocode;
+//         ObjectMapper mapper = new ObjectMapper();
+//         Geocoder geocoder = new Geocoder();
         
-        String response = geocoder.GeocodeSync(address.toString()); //"11 Wall St, New York, NY 10005");
-        JsonNode responseJsonNode = mapper.readTree(response);
+//         String response = geocoder.GeocodeSync(address.toString()); //"11 Wall St, New York, NY 10005");
+//         JsonNode responseJsonNode = mapper.readTree(response);
 
-        JsonNode items = responseJsonNode.get("items");
+//         JsonNode items = responseJsonNode.get("items");
 
-        for (JsonNode item : items) {
-            JsonNode addressInput = item.get("address");
-            String label = addressInput.get("label").asText();
-            JsonNode position = item.get("position");
+//         for (JsonNode item : items) {
+//             JsonNode addressInput = item.get("address");
+//             String label = addressInput.get("label").asText();
+//             JsonNode position = item.get("position");
 
-            String lat = position.get("lat").asText();
-            String lng = position.get("lng").asText();
-            String returnvalue = "{title: "+ label + ",\n" + "location: { lat:" + lat + ", lng:" + lng + "}";
-            System.out.println(returnvalue);
-            geocode = returnvalue;
-        }
-        return geocode;
-    }
-}
+//             String lat = position.get("lat").asText();
+//             String lng = position.get("lng").asText();
+//             String returnvalue = "{title: "+ label + ",\n" + "location: { lat:" + lat + ", lng:" + lng + "}";
+//             System.out.println(returnvalue);
+//             geocode = returnvalue;
+//         }
+//         return geocode;
+//     }
+// }
--- WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/geocoder/Geocoder.java
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/geocoder/Geocoder.java
@@ -1,32 +1,32 @@
-package routeplannerpoc.windesheimdemo.geocoder;
+// package routeplannerpoc.windesheimdemo.geocoder;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
+// import java.io.IOException;
+// import java.net.URI;
+// import java.net.URLEncoder;
+// import java.net.http.HttpClient;
+// import java.net.http.HttpRequest;
+// import java.net.http.HttpResponse;
+// import java.time.Duration;
 
-public class Geocoder {
+// public class Geocoder {
 
-    private static final String GEOCODING_RESOURCE = "https://geocode.search.hereapi.com/v1/geocode";
-    private static final String API_KEY = "m24PtEXA18fExM6A8XX7mae_WyR3LkBl-bGYxW8p_sk";
+//     private static final String GEOCODING_RESOURCE = "https://geocode.search.hereapi.com/v1/geocode";
+//     private static final String API_KEY = "m24PtEXA18fExM6A8XX7mae_WyR3LkBl-bGYxW8p_sk";
 
-    public String GeocodeSync(String query) throws IOException, InterruptedException {
+//     public String GeocodeSync(String query) throws IOException, InterruptedException {
 
-        HttpClient httpClient = HttpClient.newHttpClient();
+//         HttpClient httpClient = HttpClient.newHttpClient();
 
-        String encodedQuery = URLEncoder.encode(query,"UTF-8");
-        String requestUri = GEOCODING_RESOURCE + "?apiKey=" + API_KEY + "&q=" + encodedQuery;
+//         String encodedQuery = URLEncoder.encode(query,"UTF-8");
+//         String requestUri = GEOCODING_RESOURCE + "?apiKey=" + API_KEY + "&q=" + encodedQuery;
 
-        HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
-                .timeout(Duration.ofMillis(2000)).build();
+//         HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
+//                 .timeout(Duration.ofMillis(2000)).build();
 
-        HttpResponse geocodingResponse = httpClient.send(geocodingRequest,
-                HttpResponse.BodyHandlers.ofString());
+//         HttpResponse geocodingResponse = httpClient.send(geocodingRequest,
+//                 HttpResponse.BodyHandlers.ofString());
 
-        return (String) geocodingResponse.body();
-    }
+//         return (String) geocodingResponse.body();
+//     }
 
-}
+// }
