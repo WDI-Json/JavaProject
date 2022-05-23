c2ae32e4411c804dd221b5995e2daf2c0abc56e4
WDI_JSON <wouterdijks@hotmail.com>
2022-05-23 17:45:48 +0200

    Dijkstra implement maar niet gekoppeld

--- WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/Dijkstra/Dijkstra.java
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/Dijkstra/Dijkstra.java
@@ -4,54 +4,70 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.lang.Math;
 
 public class Dijkstra {
 
-    public static Graph calculateShortestPathFromSource(Graph graph, Node source) {
+  public static Graph calculateShortestPathFromSource(Graph graph, Node source) {
 
-        source.setDistance(0);
+    source.setDistance(0);
 
-        Set<Node> settledNodes = new HashSet<>();
-        Set<Node> unsettledNodes = new HashSet<>();
-        unsettledNodes.add(source);
+    Set<Node> settledNodes = new HashSet<>();
+    Set<Node> unsettledNodes = new HashSet<>();
+    unsettledNodes.add(source);
 
-        while (unsettledNodes.size() != 0) {
-            Node currentNode = getLowestDistanceNode(unsettledNodes);
-            unsettledNodes.remove(currentNode);
-            for (Entry<Node, Integer> adjacencyPair : currentNode.getAdjacentNodes().entrySet()) {
-                Node adjacentNode = adjacencyPair.getKey();
-                Integer edgeWeigh = adjacencyPair.getValue();
+    while (unsettledNodes.size() != 0) {
+      Node currentNode = getLowestDistanceNode(unsettledNodes);
+      unsettledNodes.remove(currentNode);
+      for (Entry<Node, Integer> adjacencyPair : currentNode.getAdjacentNodes().entrySet()) {
+        Node adjacentNode = adjacencyPair.getKey();
+        Integer edgeWeigh = adjacencyPair.getValue();
 
-                if (!settledNodes.contains(adjacentNode)) {
-                    CalculateMinimumDistance(adjacentNode, edgeWeigh, currentNode);
-                    unsettledNodes.add(adjacentNode);
-                }
-            }
-            settledNodes.add(currentNode);
+        if (!settledNodes.contains(adjacentNode)) {
+          CalculateMinimumDistance(adjacentNode, edgeWeigh, currentNode);
+          unsettledNodes.add(adjacentNode);
         }
-        return graph;
+      }
+      settledNodes.add(currentNode);
     }
+    return graph;
+  }
 
-    private static void CalculateMinimumDistance(Node evaluationNode, Integer edgeWeigh, Node sourceNode) {
-        Integer sourceDistance = sourceNode.getDistance();
-        if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
-            evaluationNode.setDistance(sourceDistance + edgeWeigh);
-            LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
-            shortestPath.add(sourceNode);
-            evaluationNode.setShortestPath(shortestPath);
-        }
+  private static void CalculateMinimumDistance(Node evaluationNode, Integer edgeWeigh, Node sourceNode) {
+    Integer sourceDistance = sourceNode.getDistance();
+    if (sourceDistance + edgeWeigh < evaluationNode.getDistance()) {
+      evaluationNode.setDistance(sourceDistance + edgeWeigh);
+      LinkedList<Node> shortestPath = new LinkedList<>(sourceNode.getShortestPath());
+      shortestPath.add(sourceNode);
+      evaluationNode.setShortestPath(shortestPath);
     }
+  }
 
-    private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
-        Node lowestDistanceNode = null;
-        int lowestDistance = Integer.MAX_VALUE;
-        for (Node node : unsettledNodes) {
-            int nodeDistance = node.getDistance();
-            if (nodeDistance < lowestDistance) {
-                lowestDistance = nodeDistance;
-                lowestDistanceNode = node;
-            }
-        }
-        return lowestDistanceNode;
+  private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
+    Node lowestDistanceNode = null;
+    int lowestDistance = Integer.MAX_VALUE;
+    for (Node node : unsettledNodes) {
+      int nodeDistance = node.getDistance();
+      if (nodeDistance < lowestDistance) {
+        lowestDistance = nodeDistance;
+        lowestDistanceNode = node;
+      }
     }
+    return lowestDistanceNode;
+  }
+
+  public double CalculateDistance(double lat1, double lon1, double lat2, double lon2, char unit) {
+    double theta = lon1 - lon2;
+    double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
+        + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+    dist = Math.acos(dist);
+    dist = Math.toRadians(dist);
+    dist = dist * 60 * 1.1515;
+    if (unit == 'K') {
+      dist = dist * 1.609344;
+    } else if (unit == 'N') {
+      dist = dist * 0.8684;
+    }
+    return dist;
+  }
 }
