9cd110cd43159fdf6773f29ef97a07d973e4ef58
WDI_JSON <wouterdijks@hotmail.com>
2022-05-27 11:36:15 +0200

    Unittesting en prepare for video

--- WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/WindesheimDemoApplication.java
+++ WindesheimDemo/src/main/java/routeplannerpoc/windesheimdemo/WindesheimDemoApplication.java
@@ -2,22 +2,9 @@ package routeplannerpoc.windesheimdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-import org.springframework.context.annotation.ComponentScan;
-
-import routeplannerpoc.windesheimdemo.Dijkstra.Node;
-import routeplannerpoc.windesheimdemo.Dijkstra.Dijkstra;
-import routeplannerpoc.windesheimdemo.model.Customer;
-import routeplannerpoc.windesheimdemo.model.Order;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-@SpringBootApplication
-@ComponentScan(basePackages = { "routeplannerpoc.windesheimdemo" })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WindesheimDemoApplication {
   public static void main(String[] args) {
     SpringApplication.run(WindesheimDemoApplication.class, args);
